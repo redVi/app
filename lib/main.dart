17c22ad31df9b47aa385b3c91dfd3114e1dd2007
redVi <liberty195@gmail.com>
2022-10-13 15:31:27 +0300

    sorting imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+
+// Package imports:
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const App());
